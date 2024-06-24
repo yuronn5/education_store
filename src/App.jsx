1ed8fb8ed2bfcdbb5320466e9e84ca1d8a821530
yurii bilous <yurii.b@elogic.com.ua>
2024-06-24 19:52:48 +0200

    testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <About />
         <Title subtitle='Gallery' title='Campus photos' />
         <Campus />
+        <Title subtitle='Testimonials' title='What students says' />
+        <Testimonials />
       </div>
     </div>
   )
