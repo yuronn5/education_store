d244656352b69a01a09682a8decf0f59869ee525
yurii bilous <yurii.b@elogic.com.ua>
2024-06-24 19:04:14 +0200

    program component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   )
 }
