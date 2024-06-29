a4ec679af849c875e408d631a84deccbf4f7a4c6
yurii bilous <yurii.b@elogic.com.ua>
2024-06-29 21:08:24 +0200

    footer comp

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Testimonials />
         <Title subtitle='Contact us' title='Get in touch' />
         <Contact />
+        <Footer />
       </div>
     </div>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>© 2024 Edusity. All rights reserved.</p>
+        <ul>
+            <li>Trems of Services</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
