314a27cf7328ae0e73774d456b126f85007b6f21
yurii bilous <yurii.b@elogic.com.ua>
2024-06-29 21:26:25 +0200

    navbar changes and added media query

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,36 @@
-
-import './Navbar.css';
+import React,{useEffect, useState} from 'react'
+import './Navbar.css'
+import {Link} from 'react-scroll'
 import logo from '../../assets/logo.png'
-import { useEffect } from 'react';
-import { useState } from 'react';
-
+import menu_icon from '../../assets/menu-icon.png'
 const Navbar = () => {
+    const [sticky, setSticky] = useState(false);
 
-  const [sticky, setSticky] = useState(false);
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            window.scrollY > 50 ? setSticky(true) : setSticky(false);
+        })
+    },[]);
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-        setSticky(true);
-      } else {
-        setSticky(false);
-      }
-    })
-  }, []);
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const toggleMenu = () =>{
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
 
-  return (
-    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-        <img src={logo} alt="logo" className="logo" />
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact us</button></li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className= {`container ${sticky ? 'dark-nav': ''}`}>
+            <img src={logo} alt="" className='logo' />
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link> us</li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
+            </ul>
+            <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
+        </nav>
+    )
 }
 
 export default Navbar
